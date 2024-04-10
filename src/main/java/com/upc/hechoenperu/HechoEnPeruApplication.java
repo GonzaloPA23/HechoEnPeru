75209790a8c1b5ba594ff5fea13955802c00f81c
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-04-10 02:11:50 -0500

    feat: Project start-up and dependencies

---
+++ src/main/java/com/upc/hechoenperu/HechoEnPeruApplication.java
@@ -0,0 +1,13 @@
+package com.upc.hechoenperu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HechoEnPeruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HechoEnPeruApplication.class, args);
+	}
+
+}
