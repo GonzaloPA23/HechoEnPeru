e7ecfb479712129ce5a96d63f3abb907c0ba9d93
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-04-11 02:41:58 -0500

    feat: CRUD complete for LocalCraftsmen

---
+++ src/main/java/com/upc/hechoenperu/controllers/LocalCraftsmanController.java
@@ -0,0 +1,55 @@
+package com.upc.hechoenperu.controllers;
+
+import com.upc.hechoenperu.dtos.LocalCraftsmanDTO;
+import com.upc.hechoenperu.entities.LocalCraftsman;
+import com.upc.hechoenperu.services.LocalCraftsmanService;
+import com.upc.hechoenperu.util.DTOConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api") // http://localhost:8080/api
+public class LocalCraftsmanController {
+    @Autowired
+    private LocalCraftsmanService localCraftsmanService;
+    @Autowired
+    private DTOConverter dtoConverter;
+
+    // Method Create Local Craftsman
+    @PostMapping("/localCraftsman") // http://localhost:8080/api/localCraftsman
+    public ResponseEntity<LocalCraftsmanDTO> insert(@RequestBody LocalCraftsmanDTO localCraftsmanDTO){
+        LocalCraftsman localCraftsman = dtoConverter.convertToEntity(localCraftsmanDTO, LocalCraftsman.class);
+        localCraftsman = localCraftsmanService.insert(localCraftsman);
+        localCraftsmanDTO = dtoConverter.convertToDto(localCraftsman, LocalCraftsmanDTO.class);
+        return new ResponseEntity<>(localCraftsmanDTO, HttpStatus.OK);
+    }
+
+    // Method Read Local Craftsman
+    @GetMapping("/localCraftsmen") // http://localhost:8080/api/localCraftsmen
+    public ResponseEntity<List<LocalCraftsmanDTO>> list(){
+        List<LocalCraftsman> localCraftsmen = localCraftsmanService.list();
+        List<LocalCraftsmanDTO> localCraftsmanDTOs = localCraftsmen.stream().map(localCraftsman -> dtoConverter.convertToDto(localCraftsman, LocalCraftsmanDTO.class)).toList();
+        return new ResponseEntity<>(localCraftsmanDTOs, HttpStatus.OK);
+    }
+
+    //Method Update Local Craftsman
+    @PutMapping("/localCraftsman") // http://localhost:8080/api/localCraftsman
+    public ResponseEntity<LocalCraftsmanDTO> update(@RequestBody LocalCraftsmanDTO localCraftsmanDTO) throws Exception {
+        LocalCraftsman localCraftsman = dtoConverter.convertToEntity(localCraftsmanDTO, LocalCraftsman.class);
+        localCraftsman = localCraftsmanService.update(localCraftsman);
+        localCraftsmanDTO = dtoConverter.convertToDto(localCraftsman, LocalCraftsmanDTO.class);
+        return new ResponseEntity<>(localCraftsmanDTO, HttpStatus.OK);
+    }
+
+    //Method Delete Local Craftsman
+    @DeleteMapping("/localCraftsman/{id}") // http://localhost:8080/api/localCraftsman/1
+    public ResponseEntity<String> delete(@PathVariable Long id) throws Exception {
+        localCraftsmanService.delete(id);
+        return new ResponseEntity<>("Local craftsman deleted", HttpStatus.OK);
+    }
+}
--- src/main/java/com/upc/hechoenperu/services/LocalCraftsmanService.java
+++ src/main/java/com/upc/hechoenperu/services/LocalCraftsmanService.java
@@ -1,4 +1,40 @@
 package com.upc.hechoenperu.services;
 
+import com.upc.hechoenperu.entities.LocalCraftsman;
+import com.upc.hechoenperu.repositories.LocalCraftsmanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class LocalCraftsmanService {
+    @Autowired
+    private LocalCraftsmanRepository localCraftsmanRepository;
+
+    // Insert a new local craftsman
+    public LocalCraftsman insert(LocalCraftsman localCraftsman){
+        return localCraftsmanRepository.save(localCraftsman);
+    }
+
+    // List all local craftsmen
+    public List<LocalCraftsman> list(){
+        return localCraftsmanRepository.findAll();
+    }
+
+    // Search a local craftsman by id
+    public LocalCraftsman searchId(Long id) throws Exception{
+        return localCraftsmanRepository.findById(id).orElseThrow(() -> new Exception("Local craftsman not found"));
+    }
+
+    // Update a local craftsman
+    public LocalCraftsman update(LocalCraftsman localCraftsman) throws Exception{
+        searchId(localCraftsman.getId());
+        return localCraftsmanRepository.save(localCraftsman);
+    }
+
+    // Delete a local craftsman
+    public void delete(Long id) throws Exception{
+        localCraftsmanRepository.delete(searchId(id));
+    }
 }
