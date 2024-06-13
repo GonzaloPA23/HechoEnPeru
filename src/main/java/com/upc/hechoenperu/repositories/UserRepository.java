7af736510d4622db931eb8a972100296fa6e94ce
Gonzalo Pingo Amaya <pingoamayagonzalo@gmail.com>
2024-06-13 01:38:57 -0500

    feat: get users by Id Asc

--- src/main/java/com/upc/hechoenperu/repositories/UserRepository.java
+++ src/main/java/com/upc/hechoenperu/repositories/UserRepository.java
@@ -15,7 +15,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
     Boolean existsByEmail(String email);
     @Query("SELECT u FROM User u JOIN u.roles r WHERE r.nameRole = 'USER' AND u.id = :id")
     User findUserById(Long id);
-    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.nameRole = 'USER'")
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.nameRole = 'USER' ORDER BY u.id ASC")
     List<User> list();
     //@Query(value = "SELECT users.id, name, last_name, email, r.name_role, date_created,enabled FROM users JOIN users_roles ur on users.id = ur.user_id JOIN roles r on ur.role_id = r.id OFFSET :offset LIMIT :limit", nativeQuery = true)
     @Query("SELECT u FROM User u JOIN u.roles r WHERE r.nameRole = 'USER' order by u.id ASC")
